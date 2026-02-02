7a4f373834aba35eb6b73432528b0130e89658a8
Mengzhongren <1217820711@qq.com>
2026-02-02 22:40:50 +0800

    change

--- 02_Two_Point/283.移动零.py
+++ 02_Two_Point/283.移动零.py
@@ -11,6 +11,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        n = len(nums)
+        if n <= 1:
+            return nums
         
+        slow,fast = 0,0
+        while fast < n:
+            if nums[fast] != 0:
+                nums[slow],nums[fast] = nums[fast],nums[slow]
+                slow += 1
+            fast += 1
+        
+        return nums
+
+# 时间复杂度:O(N),其中N是数组的长度。我们只需要对数组进行一次遍历。
+# 空间复杂度:O(1),我们只需要常数的空间存放    
 # @lc code=end
 
--- 02_Two_Point/42.接雨水.py
+++ 02_Two_Point/42.接雨水.py
@@ -11,6 +11,27 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        if not height:
+            return 0
+        left = 0
+        right = len(height) - 1
+        left_max = height[0]
+        right_max = height[-1]
+        res = 0
+
+        while left < right:
+            left_max = max(left_max,height[left])
+            right_max = max(right_max,height[right])
+
+            if left_max < right_max:
+                res += left_max - height[left]
+                left += 1
+            else:
+                res += right_max - height[right]
+                right -= 1
         
+        return res
+# 时间复杂度:O(N),因为每个i被且仅被一个指针遍历一次
+# 空间复杂度:O(1),只使用了常数个变量        
 # @lc code=end
 
